c7f0b212fbde70532540c614df088cf4ab3a0da9
ChasingWind98 <614559951@qq.com>
2020-02-05 16:11:40 +0800

    提交测试

---
+++ serendipy/src/main/java/edu/ahnu/Draw.java
@@ -0,0 +1,13 @@
+package edu.ahnu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Draw extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX程序");
+        primaryStage.show();
+    }
+}
