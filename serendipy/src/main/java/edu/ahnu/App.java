a19f1b716f25bbcebe58574266c3056fb368f30f
ChasingWind <614559951@qq.com>
2021-07-20 21:44:32 +0800

    master分支修改

--- serendipy/src/main/java/edu/ahnu/App.java
+++ serendipy/src/main/java/edu/ahnu/App.java
@@ -45,7 +45,6 @@ public class App extends Application {
 
         StageMapUtil.STAGE.put("topStage", primaryStage);
 
-        System.out.println("test hot-fix333333");
         System.out.println("test hot-fix22");
     }
 }
